e7d0b29958be29fad260c486f5ec423c43e4efe3
alejandrodh <alejandro@digitalhouse.com>
2022-07-07 16:08:23 -0300

    carpetas en database

---
+++ database/config/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  //Comentados los valores para windows
+  "development": {
+    "username": "root",
+    "password": "root", //null
+    "database": "movies_db",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
